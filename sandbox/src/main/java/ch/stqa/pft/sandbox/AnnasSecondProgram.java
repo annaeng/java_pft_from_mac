17054e09654e4dd0d853b61cbe0708bfbaa4b828
annaeng <engel.anna@gmx.ch>
2017-03-19 16:03:54 +0100

    Изменение привязки проекта к репозиторию

--- sandbox/src/main/java/ch/stqa/pft/sandbox/AnnasSecondProgram.java
+++ sandbox/src/main/java/ch/stqa/pft/sandbox/AnnasSecondProgram.java
@@ -1,9 +1,8 @@
 package ch.stqa.pft.sandbox;
 
 public class AnnasSecondProgram {
-
-  public static void main (String [] args) {
-    System.out.println("Hello, Anna's Second Program!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello, Anna's Second Program!");
+    }
 
 }
