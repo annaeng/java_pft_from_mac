cd27e0c3f38bc3694f9d6415622870f8f52c4db4
annaeng <engel.anna@gmx.ch>
2017-03-20 23:52:53 +0100

    Вычислила расстояние между точками линейное

--- sandbox/src/main/java/ch/stqa/pft/sandbox/AnnasProgramPoint.java
+++ sandbox/src/main/java/ch/stqa/pft/sandbox/AnnasProgramPoint.java
@@ -3,10 +3,43 @@ package ch.stqa.pft.sandbox;
 public class AnnasProgramPoint {
 
   public static void main(String[] args) {
-
-    System.out.println("УРРРРРРАААААА!!!! ");
+    //distance(5, 3);
+    // в круглых скобках значения которые передаются в параметры функции distance
+    double pen1 = 5;
+    double pen2 = 3;
+    System.out.println("Расстояние между точками = " + distance (pen1, pen2) );
 
   }
 
+  public static double distance(double p1, double p2) {
+    // в круглых скобках параметры функции distance
+    return p1 - p2;
+
+  }
 }
 
+
+/**public static double distance(Point p1, Point p2) {
+ которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
+
+ //3. Сделать запускаемый класс, то есть содержащий функцию public static void main(String[] args) {...}
+ //и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
+
+ String distance = "Расстояние между точками ";
+ double p1 = 1;
+ double p2 = 5;
+ double d = p2 - p1;
+ System.out.println(distance + p1 + " и " + p2 + " = " + d);
+ */
+/**public static double distance(Point p1, Point p2) {
+ которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
+
+ //3. Сделать запускаемый класс, то есть содержащий функцию public static void main(String[] args) {...}
+ //и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
+
+ String distance = "Расстояние между точками ";
+ double p1 = 1;
+ double p2 = 5;
+ double d = p2 - p1;
+ System.out.println(distance + p1 + " и " + p2 + " = " + d);
+ */
