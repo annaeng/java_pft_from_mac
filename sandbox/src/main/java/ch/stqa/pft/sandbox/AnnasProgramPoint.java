871683b126cf0fd758759b8824ac99a2b9477be1
annaeng <engel.anna@gmx.ch>
2017-03-21 20:42:07 +0100

    Задание 2: (вычисление расстояния между двумя точками) при помощи метода в классе Point

--- sandbox/src/main/java/ch/stqa/pft/sandbox/AnnasProgramPoint.java
+++ sandbox/src/main/java/ch/stqa/pft/sandbox/AnnasProgramPoint.java
@@ -3,44 +3,19 @@ package ch.stqa.pft.sandbox;
 public class AnnasProgramPoint {
 
   public static void main(String[] args) {
-    //distance(5, 3);
-    // в круглых скобках значения которые передаются в параметры функции distance
 
-    Distance d = new Distance(5,3);
+    Point p1 = new Point (2, 6);
+    Point p2 = new Point (4, 3);
 
-    System.out.println("Расстояние между точками " + d.p1 + " и " + d.p2 + " равно " + distance (d) );
-
+    System.out.println("Расстояние между точкой с координатами " + "x = " + p1.x + " ; " + "y = " + p1.y + " и " +
+            "точкой с координатами " + "x = " + p2.x + " ; " + "y = " + p2.y + " равно " + distance (p1, p2) );
   }
 
-  public static double distance(Distance d ) {
-    // в круглых скобках параметры функции distance
-    return d.p1 - d.p2;
+  public static double distance (Point p1, Point p2) {
 
+    return Math.sqrt( (p2.x - p1.x)*(p2.x - p1.x)+ (p1.y - p2.y)*(p1.y - p2.y) );
   }
 }
 
 
-/**public static double distance(Point p1, Point p2) {
- которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
 
- //3. Сделать запускаемый класс, то есть содержащий функцию public static void main(String[] args) {...}
- //и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
-
- String distance = "Расстояние между точками ";
- double p1 = 1;
- double p2 = 5;
- double d = p2 - p1;
- System.out.println(distance + p1 + " и " + p2 + " = " + d);
- */
-/**public static double distance(Point p1, Point p2) {
- которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
-
- //3. Сделать запускаемый класс, то есть содержащий функцию public static void main(String[] args) {...}
- //и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
-
- String distance = "Расстояние между точками ";
- double p1 = 1;
- double p2 = 5;
- double d = p2 - p1;
- System.out.println(distance + p1 + " и " + p2 + " = " + d);
- */
---
+++ sandbox/src/main/java/ch/stqa/pft/sandbox/Point.java
@@ -0,0 +1,25 @@
+package ch.stqa.pft.sandbox;
+
+/**
+ * Created by annaryapolova on 21.03.17.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+
+  public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
+
+
+}
+
+/**2. Создать функцию
+public static double distance(Point p1, Point p2)
+        которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
+ */
