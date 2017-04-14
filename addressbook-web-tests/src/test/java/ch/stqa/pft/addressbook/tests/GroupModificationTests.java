53bd76c26e4cd42e31af9145dcfeb80f66a4edeb
annaeng <engel.anna@gmx.ch>
2017-04-14 21:54:48 +0200

    4.5 Теперь сравниваем не количество элементов, а количество списков во всех тестах групп

--- addressbook-web-tests/src/test/java/ch/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ch/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import ch.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 /**
  * Created by annaryapolova on 08.04.17.
  */
@@ -12,17 +14,17 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup (new GroupData("о, святые печеньки1!!", null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() -1 );
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupCreation(new GroupData("о, святые печеньки1", "о, святые печеньки3", "о, святые печеньки3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before );
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() );
   }
 
 
