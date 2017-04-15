34cf3d6c629e5d583321d165ba08d1e00916c79e
annaeng <engel.anna@gmx.ch>
2017-04-15 11:52:15 +0200

    4.6. Реализовано сравнение списков групп целиком до и после удаления

--- addressbook-web-tests/src/test/java/ch/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ch/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -11,8 +11,8 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().goToGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()) {
-      app.getGroupHelper().createGroup (new GroupData("о, святые печеньки1!!", null, null));
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("о, святые печеньки1!!", null, null));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
@@ -21,6 +21,8 @@ public class GroupDeletionTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
 
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
