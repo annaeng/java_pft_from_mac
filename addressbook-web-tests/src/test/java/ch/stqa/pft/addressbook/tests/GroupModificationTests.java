2f8fa0288ecc1bb1d251283ffcadb72cef698d4b
annaeng <engel.anna@gmx.ch>
2017-04-22 14:58:39 +0200

    5.4. Реализован fluent interface для групп

--- addressbook-web-tests/src/test/java/ch/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ch/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -17,7 +17,7 @@ public class GroupModificationTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("о, святые печеньки1!!", null, null));
+      app.group().create(new GroupData().withName("о, святые печеньки1!!"));
     }
   }
 
@@ -26,7 +26,8 @@ public class GroupModificationTests extends TestBase {
 
     List<GroupData> before = app.group().list();
     int index = before.size() -1;
-    GroupData group = new GroupData(before.get(index).getId(),"о, святые печеньки1", "о, святые печеньки3", "о, святые печеньки3");
+    GroupData group = new GroupData().withId(before.get(index).getId()).
+            withName("о, святые печеньки3").withHeader("о, святые печеньки3").withFooter("о, святые печеньки3");
 
     app.group().modify(index, group);
 
